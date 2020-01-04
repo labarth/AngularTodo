0319a8888b642460906a90086d2f89e60634f4eb
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-04 17:52:07 +0300

    add todo-list markups and base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './todo-list/list/list.component';
+import { FormComponent } from './todo-list/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
